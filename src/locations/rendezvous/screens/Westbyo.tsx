5b124ee6983e925c6f1b8aac53b9ce9c33793cb4
lileeluna <elliechan.nhs@gmail.com>
2026-06-27 15:02:00 -0700

    fix: build errors; temporarily removed unused variables

--- src/locations/rendezvous/screens/Westbyo.tsx
+++ src/locations/rendezvous/screens/Westbyo.tsx
@@ -7,7 +7,7 @@ type RendezvousDisplayProps = {
   screenType: string;
 };
 
-export const RendezvousDisplay = ({ data, station, screenType }: RendezvousDisplayProps) => {
+export const RendezvousDisplay = ({ data, station }: RendezvousDisplayProps) => {       // removed screenType due to build errors; may need to add back later
 
   return (
     <WestBYOScreen
--- src/locations/rendezvous/templates/WestByoTemplate.tsx
+++ src/locations/rendezvous/templates/WestByoTemplate.tsx
@@ -29,7 +29,7 @@ type CompactPriceListProps = {
   iconSize?: string;
 };
 
-export default function WestBYOScreen({ data, station }: WestBYOScreenProps): ReactElement {
+export default function WestBYOScreen({ data }: WestBYOScreenProps): ReactElement {     // removed station due to build errors; may need to add back later
   console.log("data:", data)
   const title = "BUILD YOUR OWN ENTRÉE";
   const styleItems = getStationItems(data, "STYLE");
